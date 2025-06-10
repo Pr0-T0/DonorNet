85483db9765f65849a640c9a5ca13b908794db3e
Pr0-T0 <sarathsasidharan557@gmail.com>
2025-06-10 08:16:18 +0000

    logo issue fixed

--- src/components/LogoSection.tsx
+++ src/components/LogoSection.tsx
@@ -1,8 +1,9 @@
+import logo from '../assets/logo.png';
 const LogoSection = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="w-40 h-40 rounded-full bg-gray-300 flex items-center justify-center text-sm font-semibold">
-        <img src="src/assets/logo.png" alt="Donor-Net" className="" />
+        <img src={logo} alt="Donor-Net" className="" />
       </div>
       {/* <h1 className="mt-4 text-red-600 font-bold text-xl">Donor-Net</h1> */}
       {/* <span className="text-red-500 text-sm mt-1">tagline</span> */}
